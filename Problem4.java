376afc903636e182ab0c71f2da0a845d0e58d406
ANUJ-a1-14 <24lulua@rbunagpur.in>
2025-09-13 20:31:44 +0530

    PROBLEM-4

---
+++ Problem4.java
@@ -0,0 +1,24 @@
+public class Problem4 {
+    public static int findMinSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int j = 1; j < n; j++) dp[0][j] = dp[0][j - 1] + grid[0][j];
+        for (int i = 1; i < m; i++) dp[i][0] = dp[i - 1][0] + grid[i][0];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+            {10, 8, 2},
+            {10, 5, 1000},
+            {1, 1, 2}
+        };
+        System.out.println("Minimum: " + findMinSum(grid));
+    }
+}
