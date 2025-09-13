51cb13a7c967f35b7ebf36baa768cf5214478e93
ANUJ760 <anujngp006@gmail.com>
2025-09-13 19:54:54 +0530

    PROBLEM-1

---
+++ Problem1.java
@@ -0,0 +1,24 @@
+class longestIncrSubsqnc{
+    public static int lis(int[] arr){
+        int n = arr.length;
+        int[] dp = new int[n];
+        int len = 1;
+        for(int i=0; i<n; i++){
+            dp[i]=1;
+        }
+        for(int i = 0; i<n; i++){
+            for (int j=0; j<i; j++){
+                if(arr[j]<arr[i]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            len = Math.max(dp[i], len);
+
+        }
+        return len;
+    }
+    public static void main(String[] args){
+        int[] nums = {2,3,1};
+         System.out.println("Length: " + lis(nums)); 
+    }
+}
